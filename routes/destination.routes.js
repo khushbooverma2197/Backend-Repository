48b5bf62895dd2ec741ec44c4a66f20b28ecafae
khushbooverma2197 <khushbooverma2197@gmail.com>
2026-02-28 15:54:56 +0530

    Update destination.routes.js

--- routes/destination.routes.js
+++ routes/destination.routes.js
@@ -6,6 +6,12 @@ const authMiddleware = require('../middleware/auth.middleware');
 
 // Get all destinations
 router.get('/', destinationController.getAllDestinations);
+// Search destinations with filters
+router.get('/search', destinationController.searchDestinations);
+// Get personalized recommendations
+router.post('/recommendations', destinationController.getPersonalizedRecommendations);
+// Get budget estimate for a destination
+router.get('/:destinationId/budget', destinationController.getBudgetEstimate);
 // Get destination by ID
 router.get('/:id', destinationController.getDestinationById);
 // Create new destination (protected)
