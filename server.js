7921c0a937e56d8aaabb29f2d125f522ddc39b73
khushbooverma2197 <khushbooverma2197@gmail.com>
2026-03-04 01:16:16 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ require('dotenv').config();
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // Import and use routes
 const authRoutes = require('./routes/auth.routes');
